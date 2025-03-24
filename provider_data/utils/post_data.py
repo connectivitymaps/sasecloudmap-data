f38dc9f674204ec4262e86c0ee7c688a81cca194
Jonas Wagner <pew@users.noreply.github.com>
2025-03-24 12:43:05 +0000

    promote dev to prod #2

--- provider_data/utils/post_data.py
+++ provider_data/utils/post_data.py
@@ -16,24 +16,25 @@ def write_and_post(
         "auth": os.environ["AUTH"],
         "content-type": "applicatjon/json",
     }
+    payload = {}
+    payload["friendlyName"] = friendly_name
+    payload["appType"] = app_type
 
-    with open(f"output/{provider_name}.json") as f:
-        payload = {}
-        payload["data"] = json.load(f)
-        payload["friendlyName"] = friendly_name
-        payload["appType"] = app_type
-
-        if update_dev:
+    if update_dev:
+        with open(f"output/{provider_name}.json") as f:
+            payload["data"] = json.load(f)
             dev = httpx.post(
                 f"{os.environ['DEV_HOSTNAME']}{provider_name}",
                 headers=headers,
                 json=payload,
             )
             print(f"dev update: {dev.text}")
-        if update_prod:
-            prod = httpx.post(
-                f"{os.environ['PROD_HOSTNAME']}{provider_name}",
-                headers=headers,
-                json=payload,
-            )
-            print(f"prod update: {prod.text}")
+    if update_prod:
+        dev = httpx.get(f"{os.environ['DEV_HOSTNAME']}{provider_name}")
+        payload["data"] = dev.json()
+        prod = httpx.post(
+            f"{os.environ['PROD_HOSTNAME']}{provider_name}",
+            headers=headers,
+            json=payload,
+        )
+        print(f"prod update: {prod.text}")
